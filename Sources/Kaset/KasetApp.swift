f09e150a495881e8fe84a5591e37006eb2a6517f
httperry <priyadarshan.m@outlook.com>
2026-08-16 00:18:52 +0530

    fix: change windowToolbarFullScreenVisibility from hidden to onHover

--- Sources/Kaset/KasetApp.swift
+++ Sources/Kaset/KasetApp.swift
@@ -250,7 +250,7 @@ struct KasetApp: App {
                 .environment(\.showCommandBar, self.$showCommandBar)
                 .environment(\.showWhatsNew, self.$showWhatsNew)
                 .environment(\.usesLegacyMacOS15UI, self.settings.useLegacyMacOS15UI)
-                .windowToolbarFullScreenVisibility(.hidden)
+                .windowToolbarFullScreenVisibility(.onHover)
                 .onAppear {
                     DiagnosticsLogger.app.info("KasetApp: App content appeared")
                     self.textInputFocusState.startMonitoring()
